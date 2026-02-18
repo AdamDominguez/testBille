b27cd539f75beafc441c68168ec48a63c0100e22
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2026-02-18 11:04:19 +0100

    Update js.js

--- js.js
+++ js.js
@@ -75,8 +75,8 @@ function handleClick() {
 }
 
 window.addEventListener("deviceorientation", (event) => {
-    bille.ax = event.gamma * 0.1;
-    bille.ay = event.beta * 0.1;
+    bille.ax = event.gamma * 0.5;
+    bille.ay = event.beta * 0.5;
 });
 
 update();
